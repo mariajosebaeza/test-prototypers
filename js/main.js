5a0ab5934c912d46571a44317876d97b00f22449
Maria Jose Baeza Sanhueza <mariajosebaezasanhueza@MacBook-Air-de-Maria.local>
2017-01-06 12:22:16 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+  $(".btn-nav").on("click tap", function() {
+    $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
+    $(this).toggleClass("animated");
+  });
+});
+
+$(document).ready(function() {
+$('#slides').superslides({
+  slide_easing: 'easeInOutCubic',
+  slide_speed: 800,
+  pagination: true,
+  hashchange: true,
+  scrollable: true
+});
+});
